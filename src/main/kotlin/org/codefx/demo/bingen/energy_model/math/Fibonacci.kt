c513cbc4c4d8a40681088b31b0017635344b14cd
nipa <nipa@codefx.org>
2017-06-21 10:45:54 +0200

    Create code and tests for Fibonacci computation

---
+++ src/main/kotlin/org/codefx/demo/bingen/energy_model/math/Fibonacci.kt
@@ -0,0 +1,19 @@
+package org.codefx.demo.bingen.energy_model.math
+
+fun fib(n: Int): Int {
+    // return 0 for negative n's
+    if (n <= 0)
+        return 0
+    if (n == 1)
+        return 1
+
+    var a = 0
+    var b = 1
+    for (i in 2..n) {
+        val new = a + b
+        a = b
+        b = new
+    }
+
+    return b
+}
---
+++ src/test/kotlin/org/codefx/demo/bingen/energy_model/math/FibonacciTest.kt
@@ -0,0 +1,63 @@
+package org.codefx.demo.bingen.energy_model.math
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class FibonacciTest {
+
+    @Test
+    fun testFibNegative() {
+        // SETUP (nothing to do here)
+
+        // EXECUTE
+        val f = fib(-5)
+
+        // ASSERT
+        assertEquals(0, f)
+    }
+
+    @Test
+    fun testFib0() {
+        // SETUP (nothing to do here)
+
+        // EXECUTE
+        val f = fib(0)
+
+        // ASSERT
+        assertEquals(0, f)
+    }
+
+    @Test
+    fun testFib1() {
+        // SETUP (nothing to do here)
+
+        // EXECUTE
+        val f = fib(1)
+
+        // ASSERT
+        assertEquals(1, f)
+    }
+
+    @Test
+    fun testFib3() {
+        // SETUP (nothing to do here)
+
+        // EXECUTE
+        val f = fib(3)
+
+        // ASSERT
+        assertEquals(2, f)
+    }
+
+    @Test
+    fun testFib7() {
+        // SETUP (nothing to do here)
+
+        // EXECUTE
+        val f = fib(7)
+
+        // ASSERT
+        assertEquals(13, f)
+    }
+
+}
