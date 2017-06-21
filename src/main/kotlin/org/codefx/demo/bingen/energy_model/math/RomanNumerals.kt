ceb12504e2a28fa868ac0c50db7bbd9c5ded3035
nipa <nipa@codefx.org>
2017-06-21 11:20:23 +0200

    Create tests for Roman Nuemrals

---
+++ src/main/kotlin/org/codefx/demo/bingen/energy_model/math/RomanNumerals.kt
@@ -0,0 +1,5 @@
+package org.codefx.demo.bingen.energy_model.math
+
+fun asRoman(n: Int): String {
+    return ""
+}
---
+++ src/test/kotlin/org/codefx/demo/bingen/energy_model/math/RomanNumeralsTest.kt
@@ -0,0 +1,87 @@
+package org.codefx.demo.bingen.energy_model.math
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class RomanNumeralsTest {
+
+    /*
+     * ALL OF THESE TESS FAIL BECAUSE 'RomanNumerals' IS INCOMPLETE.
+     * CHANGE IT SO THESE TESTS PASS; IT SUFFICES TO FO UP TO 50 / "L"
+     *
+     * FOR BACKGROUND ON ROMAN NUMERALS AND HOW TO CONVERT TO THEM:
+     * http://turner.faculty.swau.edu/mathematics/materialslibrary/roman/
+     *
+     * To make your live easier by foregoing the compaction step,
+     * include 40/XL, 9/IX, and 4/IV in the list of replacements.
+     *
+     * Remember that strings can be concatenated with a simple + operation.
+     */
+
+    @Test
+    fun asRoman1() {
+        val roman = asRoman(1)
+        assertEquals("I", roman)
+    }
+
+    @Test
+    fun asRoman3() {
+        val roman = asRoman(3)
+        assertEquals("III", roman)
+    }
+
+    @Test
+    fun asRoman4() {
+        val roman = asRoman(4)
+        assertEquals("IV", roman)
+    }
+
+    @Test
+    fun asRoman5() {
+        val roman = asRoman(5)
+        assertEquals("V", roman)
+    }
+
+    @Test
+    fun asRoman7() {
+        val roman = asRoman(7)
+        assertEquals("VII", roman)
+    }
+
+    @Test
+    fun asRoman9() {
+        val roman = asRoman(9)
+        assertEquals("IX", roman)
+    }
+
+    @Test
+    fun asRoman10() {
+        val roman = asRoman(10)
+        assertEquals("X", roman)
+    }
+
+    @Test
+    fun asRoman23() {
+        val roman = asRoman(23)
+        assertEquals("XXIII", roman)
+    }
+
+    @Test
+    fun asRoman39() {
+        val roman = asRoman(23)
+        assertEquals("XXXIX", roman)
+    }
+
+    @Test
+    fun asRoman42() {
+        val roman = asRoman(42)
+        assertEquals("XLII", roman)
+    }
+
+    @Test
+    fun asRoman50() {
+        val roman = asRoman(1)
+        assertEquals("L", roman)
+    }
+
+}
