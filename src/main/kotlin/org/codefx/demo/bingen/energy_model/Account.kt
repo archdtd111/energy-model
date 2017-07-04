3abb1bf721bee1c1153e7f0b8a053aca815d05b7
Nicolai Parlog <nipa@codefx.org>
2017-07-04 10:13:09 +0200

    Clean up Account

--- src/main/kotlin/org/codefx/demo/bingen/energy_model/Account.kt
+++ src/main/kotlin/org/codefx/demo/bingen/energy_model/Account.kt
@@ -3,28 +3,33 @@ package org.codefx.demo.bingen.energy_model
 class Account(var balance: Int, var limit: Int) {
 
     fun deposit(amount: Int): Int {
-        if (amount >= 0)  {
-            // Condition the balance is not negative
-            balance += amount
-            return amount
-        }
-        else {
-            // insufficient balance; pay out 0
+        // only positive amounts are allowed
+        if (amount < 0) {
             return 0
         }
+
+        // deposit amount
+        balance += amount
+        return amount
     }
     fun withdraw(amount: Int): Int {
-        if (limit <= balance - amount && amount > 0) {
-            // balance suffices, pay out the money
+        // only positive amounts are allowed
+        if (amount < 0) {
+            return 0
+        }
 
-            balance = balance - amount
+        if (balance - amount >= limit) {
+            // balance suffices, pay out the money
+            balance -= amount
             return amount
         } else {
             // insufficient balance; pay out 0
             return 0
         }
     }
+
     fun currentBalance(): Int {
         return balance
     }
+
 }
